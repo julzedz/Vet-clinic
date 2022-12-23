4bd1f5df45952976549700aa496299b9c4188306
Edozie <edoziechukwuma@gmail.com>
2022-12-22 18:01:47 -0600

    Add owner_id column

--- schema.sql
+++ schema.sql
@@ -25,3 +25,4 @@ PRIMARY KEY(id)
 
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
